5294c1c1298b23d502f01a05efe9d74dbc5552a1
Nina-Petit <nina.petit6@gmail.com>
2021-06-09 14:53:10 +0200

    Automatic focus when arriving on the page

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import './searchbar.scss';
 
-const SearchBar = ({ searchInputValue, onInputChange }) => (
-  <div>
+const SearchBar = ({ searchInputValue, onInputChange }) => {
+  // A l'ouverture de la page, le focus est fait automatiquement sur l'input de la searchbar
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
     <input
+      ref={inputRef}
       type="text"
       placeholder="Jurassic Park"
       value={searchInputValue}
       onChange={onInputChange}
     />
-  </div>
-);
+  );
+};
 
 SearchBar.propTypes = {
   searchInputValue: PropTypes.string.isRequired,
