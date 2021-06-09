9b079c59398fca4ea1c1f01344b0918dd05aa5de
ghsdl <maeva.ghsdl@gmail.com>
2021-06-09 20:44:33 +0200

    :zap: Sign up ok, bug sign in

--- API/index.js
+++ API/index.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 
+// SERVER
 const express = require('express');
-
+const authRouter = require('./app/routers/auth');
 const app = express();
+const cors = require('cors');
 
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+app.use('/', authRouter);
+
+// PORT
 const port = process.env.PORT || 3000;
-
 app.listen(port, _ => {
    console.log(`http://localhost:${port}`);
 });
