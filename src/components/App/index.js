90fb7cce56bd3e674f9d0b348e3db2311574bc14
Rossilia <rossilia@oclock.io>
2021-06-09 18:37:08 +0200

    menu burger

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,16 +2,13 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
-
+import MenuBurger from 'src/components/MenuBurger';
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <MenuBurger />
   </div>
 );
 
-// == Export
+// == Export  
 export default App;
