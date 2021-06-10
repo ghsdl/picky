ee89b4e0c81ea127d1474107314428f583b69a9a
ClementCHA <Clement.charlesC@gmail.com>
2021-06-10 18:18:46 +0200

    bug fix, cleaner code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
 // == Import npm
 import React from 'react';
-import Header from 'src/containers/Header';
+
 // == Import
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Header />
   </div>
 );
 
