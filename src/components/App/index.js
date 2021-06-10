b070ddc1d667819a38a9a8859bb8a21662e103f0
ClementCHA <Clement.charlesC@gmail.com>
2021-06-10 19:04:59 +0200

    Additionnal in modal

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,13 @@
 // == Import npm
 import React from 'react';
-import Header from 'src/containers/Header';
+
 // == Import
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Header />
+    
   </div>
 );
 
