80b888fb6ccb5e7f216d99379d519b020e0f3b98
Rossilia <rossilia@oclock.io>
2021-06-10 11:43:14 +0200

    App modified

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,12 @@
+// == Import npm
 import React from 'react';
-<<<<<<< HEAD
 
 // == Import
-import MenuBurger from 'src/components/MenuBurger';
-// == Composant
-const App = () => (
-  <div className="app">
-    <MenuBurger />
-=======
-import Header from 'src/containers/Header.js';
-import './styles.css';
+import './style.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-   <Header />
->>>>>>> 2f6add0d872e8d8912a49393325adf868c2fc002
   </div>
 );
 
