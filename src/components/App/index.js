e6761fb1b2a25a5475c8aefd2d8245f87dbc1c3a
Nina-Petit <nina.petit6@gmail.com>
2021-06-15 16:02:34 +0200

    Routes added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,23 @@
 // == Import npm
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 // == Import
 import './styles.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import Home from 'src/containers/Home';
 import Wish from 'src/components/Wish';
 // == Composant
 
 const App = () => (
   <div className="app">
-    <Wish />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/wish">
+        <Wish />
+      </Route>
+    </Switch>
   </div>
 );
 
