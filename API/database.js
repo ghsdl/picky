7442b2ae875d61e5efa69cc93f37fc79bc43f1b1
Sofiane2002 <pride86@hotmail.fr>
2021-06-16 14:57:17 +0200

    modif file_dataBase+delete_test.http

--- API/database.js
+++ API/database.js
@@ -1,3 +1,13 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool();
+
+
+// pour se connecter sur Heroku
+/*const { Pool } = require('pg');
+
+const pool = new Pool({
+    ssl: {
+        rejectUnauthorized: false
+    }
+});*/
