d08c2cdeec456e5a820c5c18a161ab3942ab2848
ghsdl <maeva.ghsdl@gmail.com>
2021-06-16 15:50:38 +0200

    :tada: Heroku Part3

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
-// pour se connecter sur Heroku
+// pour se connecter sur Heroku décommenter
+// pour se connecter en local commenter
 
 const pool = new Pool({
     connectionString:process.env.DATABASE_URL,
