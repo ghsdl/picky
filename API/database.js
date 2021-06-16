bd8954fd92cfbc620dc99ba9874e7a73c0a58aa7
ClementCHA <Clement.charlesC@gmail.com>
2021-06-16 11:53:25 +0200

    Fix bug

--- API/database.js
+++ API/database.js
@@ -1,3 +1,10 @@
 const { Pool } = require('pg');
 
-module.exports = new Pool();
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.DB_HOST || 'bdd',
+});
+pool.connect();
+module.exports = pool;
