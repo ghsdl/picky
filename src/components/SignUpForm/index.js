081a8331ea0000511a71494c337b540a7983a4e9
Nina-Petit <nina.petit6@gmail.com>
2021-06-16 11:11:25 +0200

    SignUp component: CSS

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import './signupform.scss';
+
 const SignUpForm = () => (
-  <form>
-    <label htmlFor="pseudo">Pseudo</label>
-    <input type="text" name="pseudo" placeholder="Pseudo" />
-    <label htmlFor="mail">Email</label>
-    <input type="email" name="mail" placeholder="Email" />
-    <label htmlFor="password">Mot de passe</label>
-    <input type="text" name="password" placeholder="Mot de passe" />
-    <label htmlFor="password-confirmation">Confirmez votre mot de passe</label>
-    <input type="text" name="password-confirmation" placeholder="Confirmation de mot de passe" />
+  <form className="signUpForm">
+    <div className="signUpForm__field">
+      <label htmlFor="pseudo">Pseudo</label>
+      <input type="text" name="pseudo" placeholder="Pseudo" />
+    </div>
+    <div className="signUpForm__field">
+      <label htmlFor="mail">Email</label>
+      <input type="email" name="mail" placeholder="Email" />
+    </div>
+    <div className="signUpForm__field">
+      <label htmlFor="password">Mot de passe</label>
+      <input type="text" name="password" placeholder="Mot de passe" />
+    </div>
+    <div className="signUpForm__field">
+      <label htmlFor="password-confirmation">Confirmez votre mot de passe</label>
+      <input type="text" name="password-confirmation" placeholder="Confirmation de mot de passe" />
+    </div>
   </form>
 );
 
