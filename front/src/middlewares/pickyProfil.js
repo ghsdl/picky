dcf599045a8d4a1bd3097bd1bcd38ea2bfd89261
ClementCHA <Clement.charlesC@gmail.com>
2021-06-24 22:50:43 +0200

    fix profil

--- front/src/middlewares/pickyProfil.js
+++ front/src/middlewares/pickyProfil.js
@@ -54,7 +54,7 @@ const profil =  (store) => (next) => (action) => {
           console.log(response)
         })
         .catch((error)=> {
-          console.trace('case Patch MiddleWare',error.data)
+          console.log('case Patch MiddleWare',error)
         });
       break;
     }
