1c225661dfe87430c8370aeed794b29336121fa3
Nina-Petit <nina.petit6@gmail.com>
2021-06-24 15:06:34 +0200

    Reset of pickyFind when the user clicks on the picky logo in the header

--- front/src/containers/Header.js
+++ front/src/containers/Header.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import Header from 'src/components/Header';
 import { logout } from 'src/actions/user';
+import { reset } from 'src/actions/suggestions';
 
 // plusiuers Reducer => Mettre le nom du reducer correspondant dans le mapStateToProps
 const mapStateToProps = (state) => ({
@@ -13,6 +14,10 @@ const mapDispatchToProps = (dispatch) => ({
     const action = logout();
     dispatch(action); 
   },
+
+  resetPickyFind: () => {
+    dispatch(reset());
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
