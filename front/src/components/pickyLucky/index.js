9398a83629fe0bd0b2d9bec3712d46e436fd8dcf
Nina-Petit <nina.petit6@gmail.com>
2021-06-30 09:47:40 +0200

    Fixed Picky Lucky Show gradient

--- front/src/components/pickyLucky/index.js
+++ front/src/components/pickyLucky/index.js
@@ -90,14 +90,20 @@ const PickyLucky = ({
             <h2 className="profileLuckyPicky-title"> Picky lucky </h2>
           </div>
           <Detailshow show={show} loading={loading}/>
-        </div>
-          <div className="pickyLucky">
-            <div className="pickyLucky__footer">
-              <Link className="pickyLucky__footer__link" to="/">FIND</Link>
+          <div className="pickyLucky__gradient">
+              <div className="pickyLucky__footer">
+                {isConnected && (
+                  <Link className="pickyLucky__footer__link" to="/mood">MOOD</Link>
+                )}
+                {!isConnected && (
+                  <Link className="pickyLucky__footer__link" to="/signUp">MOOD</Link>
+                )}
                 <p className="pickyLucky__footer__text">Continuez à explorer</p>
-              <div className="pickyLucky__footer__link" onClick={pickyLuckyShow}>AGAIN</div>
+                <Link className="pickyLucky__footer__link" to="#" onClick={pickyLuckyShow}>AGAIN</Link>
+              </div>
             </div>
-          </div>
+        </div>
+          
       </div>
       )}
     </>
